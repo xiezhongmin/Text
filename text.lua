2c3fafea9b00b55d0f3cb0927a02805abae8ca0a
谢忠敏 <364101515@qq.com>
2017-01-06 15:47:55 +0800

    update text.lua

--- text.lua
+++ text.lua
@@ -4,4 +4,22 @@ window.backgroundColor(0xDDDDDD);
 
 label = Label();
 label.frame(0,50,w, 60);
-label.text("Hello World LuaView!");
+label.text("Hello World XieZhongMin!");
+
+
+function createButton( text , x,y,w,h, callback) {
+local button = Button();
+button.frame(x,y,w,h);
+button.text(text);
+button.backgroundColor(0xeeeeee);
+button.callback( function()
+print("撸啊撸",button);
+if( callback ) then
+callback();
+end
+end);
+return button;
+}
+
+button = createButton("测试",10,10,300,60);
+
